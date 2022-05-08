ff4562d049b14bbc32baf3f5c21a91c2938e34a8
Andrey Shurygin <x1523010@ya.ru>
2022-05-08 20:27:53 +0300

    Верстка 08.05.2022 - 1h

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  if (document.querySelector('body').classList.contains('indexPage')) {
-    // Code
-  } else if (document.querySelector('body').classList.contains('aboutPage')) {
-    // Code
-  }
+  const selectFirst = document.querySelector('.calc__select_first');
+  const selectSecond = document.querySelector('.calc__select_second');
+  const selectThird = document.querySelector('.calc__select_third');
+  new Choices(selectFirst, {
+    removeItems: true,
+    itemSelectText: '',
+    shouldSort: true,
+  });
+  new Choices(selectSecond, {
+    removeItems: true,
+    itemSelectText: '',
+    shouldSort: true,
+  });
+  new Choices(selectThird, {
+    removeItems: true,
+    itemSelectText: '',
+    shouldSort: true,
+  });
 });
